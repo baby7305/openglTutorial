cc0d5e000bf7e00e6d6a46513a1842e9bb773db4
学海拾贝mac <songhaibei7305@163.com>
2018-02-24 23:05:26 +0800

    Initial Commit

---
+++ openglGlfw/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openglGlfw
+//
+//  Created by 宋海貝 on 2018/2/24.
+//  Copyright © 2018年 宋海貝. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
