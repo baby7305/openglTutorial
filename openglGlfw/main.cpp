68faac2dfe149860bbae1d283fc9e82df1cf4844
baby7305 <songhaibei7305@163.com>
2018-02-26 13:42:40 +0800

    用GL_TRIANGLE_FAN画一个正10边形

--- openglGlfw/main.cpp
+++ openglGlfw/main.cpp
@@ -1,8 +1,11 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+
+void drawCircle( GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides );
 
 int main( void )
 {
@@ -33,24 +36,13 @@ int main( void )
     glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
     glLoadIdentity( ); // same as above comment
     
-    GLfloat lineVertices[] =
-    {
-        200, 100, 0,
-        100, 300, 0,
-        500, 50, 0,
-        320, 100, 0,
-        10, 10, 0
-    };
-    
     // Loop until the user closes the window
     while ( !glfwWindowShouldClose( window ) )
     {
         glClear( GL_COLOR_BUFFER_BIT );
         
-        glEnableClientState( GL_VERTEX_ARRAY );
-        glVertexPointer( 3, GL_FLOAT, 0, lineVertices );
-        glDrawArrays( GL_LINE_LOOP, 0, 5 );
-        glDisableClientState( GL_VERTEX_ARRAY );
+        // render OpenGL here
+        drawCircle( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 120, 10 );
         
         // Swap front and back buffers
         glfwSwapBuffers( window );
@@ -62,4 +54,40 @@ int main( void )
     glfwTerminate( );
     
     return 0;
+}
+
+void drawCircle( GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides )
+{
+    int numberOfVertices = numberOfSides + 2;
+    
+    GLfloat twicePi = 2.0f * M_PI;
+    
+    GLfloat circleVerticesX[numberOfVertices];
+    GLfloat circleVerticesY[numberOfVertices];
+    GLfloat circleVerticesZ[numberOfVertices];
+    
+    circleVerticesX[0] = x;
+    circleVerticesY[0] = y;
+    circleVerticesZ[0] = z;
+    
+    for ( int i = 1; i < numberOfVertices; i++ )
+    {
+        circleVerticesX[i] = x + ( radius * cos( i *  twicePi / numberOfSides ) );
+        circleVerticesY[i] = y + ( radius * sin( i * twicePi / numberOfSides ) );
+        circleVerticesZ[i] = z;
+    }
+    
+    GLfloat allCircleVertices[( numberOfVertices ) * 3];
+    
+    for ( int i = 0; i < numberOfVertices; i++ )
+    {
+        allCircleVertices[i * 3] = circleVerticesX[i];
+        allCircleVertices[( i * 3 ) + 1] = circleVerticesY[i];
+        allCircleVertices[( i * 3 ) + 2] = circleVerticesZ[i];
+    }
+    
+    glEnableClientState( GL_VERTEX_ARRAY );
+    glVertexPointer( 3, GL_FLOAT, 0, allCircleVertices );
+    glDrawArrays( GL_TRIANGLE_FAN, 0, numberOfVertices);
+    glDisableClientState( GL_VERTEX_ARRAY );
 }
