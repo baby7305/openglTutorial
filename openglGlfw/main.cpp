47327c65695ee411411809a9d80d47ce17291b84
baby7305 <songhaibei7305@163.com>
2018-02-26 13:13:51 +0800

    GL_QUAD_STRIP多边形测试

--- openglGlfw/main.cpp
+++ openglGlfw/main.cpp
@@ -1,5 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
+#include <iostream>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -17,6 +19,9 @@ int main( void )
     // Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow( SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL );
     
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
+    
     if ( !window )
     {
         glfwTerminate( );
@@ -26,20 +31,26 @@ int main( void )
     // Make the window's context current
     glfwMakeContextCurrent( window );
     
-    float vertices[] =
-    {
-        300, 300, 0.0, // top right corner
-        0, 300, 0.0, // top left corner
-        0, 0, 0.0, // bottom left corner
-        300, 0, 0.0 // bottom right corner
-    };
-    
-    glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    glViewport( 0.0f, 0.0f, screenWidth, screenHeight ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
     glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
     glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
     glOrtho( 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1 ); // essentially set coordinate system
     glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
     glLoadIdentity( ); // same as above comment
+    
+    float vertices[] =
+    {
+        200, 300, 0.0, // top left corner
+        300, 300, 0.0, // top right corner
+        200, 250, 0.0, // bottom left corner
+        300, 250, 0.0, // bottom right corner
+        
+        320, 150, 0.0,
+        350, 200, 0.0,
+        
+        400, 50, 0.0,
+        450, 120, 0.0
+    };
     
     // Loop until the user closes the window
     while ( !glfwWindowShouldClose( window ) )
@@ -49,7 +60,7 @@ int main( void )
         // Render OpenGL here
         glEnableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're using a vertex array for fixed-function attribute
         glVertexPointer( 3, GL_FLOAT, 0, vertices ); // point to the vertices to be used
-        glDrawArrays( GL_QUADS, 0, 4 ); // draw the vertixes
+        glDrawArrays( GL_QUAD_STRIP, 0, 8 ); // draw the vertices
         glDisableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're finished using the vertex arrayattribute
         
         
@@ -64,3 +75,7 @@ int main( void )
     
     return 0;
 }
+
+
+
+
