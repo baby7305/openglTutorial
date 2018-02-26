88dfbbd5a4f41ee26f7473b381c63f031dee2991
baby7305 <songhaibei7305@163.com>
2018-02-26 13:53:51 +0800

    color颜色测试

--- openglGlfw/main.cpp
+++ openglGlfw/main.cpp
@@ -1,11 +1,10 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <math.h>
+#include <iostream>
+#include <cstdlib>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-
-void drawCircle( GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides );
 
 int main( void )
 {
@@ -20,6 +19,11 @@ int main( void )
     // Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow( SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL );
     
+    
+    
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
+    
     if ( !window )
     {
         glfwTerminate( );
@@ -29,20 +33,47 @@ int main( void )
     // Make the window's context current
     glfwMakeContextCurrent( window );
     
-    glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    glViewport( 0.0f, 0.0f, screenWidth, screenHeight ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
     glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
     glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
-    glOrtho( 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1 ); // essentially set coordinate system
+    glOrtho( 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 600 ); // essentially set coordinate system
     glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
     glLoadIdentity( ); // same as above comment
+    
+    GLfloat halfScreenWidth = SCREEN_WIDTH / 2;
+    GLfloat halfScreenHeight = SCREEN_HEIGHT / 2;
+    
+    GLfloat halfSideLength = 200;
+    
+    GLfloat vertices[] =
+    {
+        halfScreenWidth, halfScreenHeight + halfSideLength, 0.0, // top center vertex
+        halfScreenWidth - halfSideLength, halfScreenHeight - halfSideLength, 0.0, // bottom left corner
+        halfScreenWidth + halfSideLength, halfScreenHeight - halfSideLength, 0.0 // bottom right corner
+    };
+    
+    GLfloat colour[] =
+    {
+        255, 0, 0,
+        0, 255, 0,
+        0, 0, 255
+    };
     
     // Loop until the user closes the window
     while ( !glfwWindowShouldClose( window ) )
     {
         glClear( GL_COLOR_BUFFER_BIT );
         
-        // render OpenGL here
-        drawCircle( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 120, 36 );
+        // Render OpenGL here
+        glEnableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're using a vertex array for fixed-function attribute
+        glEnableClientState( GL_COLOR_ARRAY );
+        glVertexPointer( 3, GL_FLOAT, 0, vertices ); // point to the vertices to be used
+        glColorPointer( 3, GL_FLOAT, 0, colour );
+        glDrawArrays( GL_TRIANGLES, 0, 3 ); // draw the vertixes
+        glDisableClientState( GL_COLOR_ARRAY );
+        glDisableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're finished using the vertex array attribute
+        
+        
         
         // Swap front and back buffers
         glfwSwapBuffers( window );
@@ -55,43 +86,6 @@ int main( void )
     
     return 0;
 }
-
-void drawCircle( GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides )
-{
-    GLint numberOfVertices = numberOfSides + 1;
-    
-    GLfloat doublePi = 2.0f * M_PI;
-    
-    GLfloat circleVerticesX[numberOfVertices];
-    GLfloat circleVerticesY[numberOfVertices];
-    GLfloat circleVerticesZ[numberOfVertices];
-    
-    //circleVerticesX[0] = x;
-    //circleVerticesY[0] = y;
-    //circleVerticesZ[0] = z;
-    
-    for ( int i = 0; i < numberOfVertices; i++ )
-    {
-        circleVerticesX[i] = x + ( radius * cos( i * doublePi / numberOfSides ) );
-        circleVerticesY[i] = y + ( radius * sin( i * doublePi / numberOfSides ) );
-        circleVerticesZ[i] = z;
-    }
-    
-    GLfloat allCircleVertices[numberOfVertices * 3];
-    
-    for ( int i = 0; i < numberOfVertices; i++ )
-    {
-        allCircleVertices[i * 3] = circleVerticesX[i];
-        allCircleVertices[( i * 3 ) + 1] = circleVerticesY[i];
-        allCircleVertices[( i * 3 ) + 2] = circleVerticesZ[i];
-    }
-    
-    glEnableClientState( GL_VERTEX_ARRAY );
-    glVertexPointer( 3, GL_FLOAT, 0, allCircleVertices );
-    glDrawArrays( GL_LINE_STRIP, 0, numberOfVertices );
-    glDisableClientState( GL_VERTEX_ARRAY );
-}
-
 
 
 
