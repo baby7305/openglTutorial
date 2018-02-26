6830d5bebfa870970d33b15e2cc0aa4de7d39049
baby7305 <songhaibei7305@163.com>
2018-02-26 13:04:09 +0800

    GL_QUADS正方形测试

--- openglGlfw/main.cpp
+++ openglGlfw/main.cpp
@@ -26,6 +26,14 @@ int main( void )
     // Make the window's context current
     glfwMakeContextCurrent( window );
     
+    float vertices[] =
+    {
+        300, 300, 0.0, // top right corner
+        0, 300, 0.0, // top left corner
+        0, 0, 0.0, // bottom left corner
+        300, 0, 0.0 // bottom right corner
+    };
+    
     glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
     glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
     glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
@@ -33,28 +41,17 @@ int main( void )
     glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
     glLoadIdentity( ); // same as above comment
     
-    GLfloat polygonVertices[] =
-    {
-        20, 100, 0,
-        100, 300, 0,
-        500, 50, 0,
-        320, 10, 0,
-        40, 40, 0
-    };
-    
-    glPolygonMode( GL_FRONT_AND_BACK, GL_FILL ); // polygon drawing mode (GL_POINT, GL_LINE, GL_FILL)
-    
-    
     // Loop until the user closes the window
     while ( !glfwWindowShouldClose( window ) )
     {
         glClear( GL_COLOR_BUFFER_BIT );
         
-        // render OpenGL here
-        glEnableClientState( GL_VERTEX_ARRAY );
-        glVertexPointer( 3, GL_FLOAT, 0, polygonVertices );
-        glDrawArrays( GL_POLYGON, 0, 5 );
-        glDisableClientState( GL_VERTEX_ARRAY );
+        // Render OpenGL here
+        glEnableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're using a vertex array for fixed-function attribute
+        glVertexPointer( 3, GL_FLOAT, 0, vertices ); // point to the vertices to be used
+        glDrawArrays( GL_QUADS, 0, 4 ); // draw the vertixes
+        glDisableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're finished using the vertex arrayattribute
+        
         
         // Swap front and back buffers
         glfwSwapBuffers( window );
