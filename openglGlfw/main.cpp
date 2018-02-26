048c08723ce496392ef49090bedb32bc72a542e3
baby7305 <songhaibei7305@163.com>
2018-02-26 09:18:14 +0800

    画一个白色三角形

--- openglGlfw/main.cpp
+++ openglGlfw/main.cpp
@@ -23,12 +23,24 @@ int main( void )
     // Make the window's context current
     glfwMakeContextCurrent( window );
     
+    float vertices[] =
+    {
+        0, 0.5, 0.0, // top corner
+        -0.5, -0.5, 0.0, // bottom left corner
+        0.5, -0.5, 0.0 // bottom right corner
+    };
+    
     /* Loop until the user closes the window */
     while ( !glfwWindowShouldClose( window ) )
     {
         glClear( GL_COLOR_BUFFER_BIT );
         
         // Render OpenGL here
+        
+        glEnableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're using a vertex array for fixed-function attribute
+        glVertexPointer( 3, GL_FLOAT, 0, vertices ); // point to the vertices to be used
+        glDrawArrays( GL_TRIANGLES, 0, 3 ); // draw the vertixes
+        glDisableClientState( GL_VERTEX_ARRAY ); // tell OpenGL that you're finished using the vertex arrayattribute
         
         
         // Swap front and back buffers
